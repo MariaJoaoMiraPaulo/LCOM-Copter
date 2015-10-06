02da97fb84e53d9199129c50bd965f2e44e71390
Maria João Mira Paulo <MariaJoaoMiraPaulo@users.noreply.github.com>
2015-10-06 09:21:14 +0000

    Function timer_get_conf is already done

--- lab2/timer.c
+++ lab2/timer.c
@@ -22,12 +22,52 @@ void timer_int_handler() {
 
 int timer_get_conf(unsigned long timer, unsigned char *st) {
 	
-	return 1;
+	char command=0;
+	int ret;
+
+	command = command|TIMER_RB_CMD; //put 1 at the last two bits (bit 6 and bit 7)
+
+	switch (timer)
+	{
+	case 0:
+		command=command |TIMER_RB_SEL(0);
+		break;
+	case 1:
+		command=command |TIMER_RB_SEL(1);
+	case 1:
+		command=command |TIMER_RB_SEL(2);
+	}
+
+	ret=  sys_outb(TIMER_CTRL,command);
+	unsigned long res;
+
+	if (ret !=0 ) //error
+		return 1;
+
+	switch (timer)
+		{
+		case 0:
+			ret= sys_inb(TIMER_0,res);
+			break;
+		case 1:
+			ret= sys_inb(TIMER_1,res);
+		case 1:
+			ret= sys_inb(TIMER_2,res);
+		}
+
+
+	if(ret!=0)
+		return 1;
+
+	*st=res;
+
+
+	return 0;
 }
 
 int timer_display_conf(unsigned char conf) {
 	
-	return 1;
+
 }
 
 int timer_test_square(unsigned long freq) {
