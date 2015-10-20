fac5fd33741b2d896c441bb46ff42c43bf332779
up201405498 <up201405498@5e170ca0-c6cb-4c95-846c-d4fa551ab4d8>
2015-10-20 09:57:07 +0000

    Finished function test_scan, but not tested!

--- lab3/test3.c
+++ lab3/test3.c
@@ -3,7 +3,35 @@
 #include "keyboard.h"
 
 int kbd_test_scan(unsigned short ass) {
+	int ipc_status;
+	int irq_set=keyboard_subscribe_int();
+	message msg;
+	int r,scancode=0;
 
+	while( scancode != BREAK_ESC ) { /* You may want to use a different condition */
+		/* Get a request message. */
+		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ){
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) {
+			/* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+				if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+					scancode=timer_int_handler();
+					keyboard_print(scancode);
+				}
+				break;
+			default:
+				break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+		}
+	}
+
+	return 1;
 }
 int kbd_test_leds(unsigned short n, unsigned short *leds) {
 	/* To be completed */
