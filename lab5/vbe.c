795edbb7ceaf72688c33f0d73a537a0abd424cf7
up201403820 <up201403820@5e170ca0-c6cb-4c95-846c-d4fa551ab4d8>
2015-11-23 22:58:57 +0000

    Started test_controller function

--- lab5/vbe.c
+++ lab5/vbe.c
@@ -48,4 +48,35 @@ int vbe_get_mode_info(unsigned short mode, vbe_mode_info_t *vmi_p) {
 	return 0;
 }
 
+int vbe_get_mode_info_block( vbe_mode_info_t *vmi_p) {
 
+	struct reg86u r;
+
+	if(lm_init()==NULL)
+	{
+		return 1;
+	}
+
+	if (lm_alloc(sizeof(vbe_mode_info_t),&map_info)==NULL)
+	{
+		return 1;
+	}
+
+	r.u.w.ax = GET_VBE_CONTROLLER_INFORMATION;  //initialize r struc with the functions and modes pretend
+	r.u.w.es = PB2BASE(map_info.phys);
+	r.u.w.di = PB2OFF(map_info.phys);
+	r.u.b.intno = BIOS_VIDEO_CARD;
+
+	if( sys_int86(&r) != OK ) {
+		printf("vbe_get_mode_info(): sys_int86() failed \n");
+		return 1;
+	}
+
+	if (r.u.w.ax != VBE_FUNC_SUPPORTED | VBE_FUNC_CALL_SUCCE)
+	{
+		return 1;
+	}
+
+
+
+}
