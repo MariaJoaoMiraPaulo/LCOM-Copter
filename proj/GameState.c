6e9c6748104a49a1a8644afdd2776a653f944d1a
up201405498 <NunoRamos@users.noreply.github.com>
2015-11-26 17:07:42 +0000

    Files!

---
+++ proj/GameState.c
@@ -0,0 +1,7 @@
+#include "GameState.h"
+
+
+void updateGame(Copter* copter){
+	draw_copter(copter);
+	update_screen();
+}
---
+++ proj/GameState.h
@@ -0,0 +1,28 @@
+#ifndef __GAMESTATE__H
+#define __GAMESTATE__H
+
+#include "copter.h"
+
+void updateGame(Copter* copter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* __GAMESTATE_H */
