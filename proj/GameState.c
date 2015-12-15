c3d94ce9fc3b4981d36792e8e4ef5483a9db6857
up201405498 <up201405498@5e170ca0-c6cb-4c95-846c-d4fa551ab4d8>
2015-12-15 09:00:43 +0000

    libma.a

--- proj/GameState.c
+++ proj/GameState.c
@@ -6,13 +6,13 @@ int hit(Copter* copter){
 	int i;
 	for (i = copter->x; i < copter->width + copter->x; i++) {
 		if(color(i,copter->y-1)==18)
-			return 1;;
+			return 1;
 		if(color(i,copter->y+copter->height+1)==18)
 			return 1;
 	}
 
 	for (i = copter->y; i < copter->height + copter->y; i++) {
-		if(color(copter->x-1,i))
+		if(color(copter->x-1,i)==18)
 			return 1;
 		if(color(copter->x+copter->width+1,i))
 			return 1;
@@ -26,6 +26,8 @@ int updateGame(Copter* copter, Margin** margins, unsigned short *sizeOfArray, un
 	if(hit(copter)==HIT){
 		return 1;
 	}
+
+	//printf("size: %d",*sizeOfArray);
 
 	if(time>=5){
 		drawObstacle(obs);
