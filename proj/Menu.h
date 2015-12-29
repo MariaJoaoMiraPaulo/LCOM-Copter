f30502ce79701d6f169a9f9a4d828fd9b6c642db
up201405498 <NunoRamos@users.noreply.github.com>
2015-12-29 19:18:02 +0000

    Some Comments

--- proj/Menu.h
+++ proj/Menu.h
@@ -2,12 +2,18 @@
 #define __MENU_H
 
 
- unsigned long IRQ_SET_KBD;//irq_set_kbd ;
- int IRQ_SET_TIMER;//irq_set_timer;
- unsigned long IRQ_SET_MOUSE;//irq_set_mouse ;
+unsigned long IRQ_SET_KBD;
+int IRQ_SET_TIMER;
+unsigned long IRQ_SET_MOUSE;
 
+/*
+ * @brief Functions used to subscribe all interruptions need in this program
+ */
 void subscribe();
 
+/*
+ * @brief Functions used to unsubscribe all interruptions need in this program
+ */
 void unsubscribe();
 
 
