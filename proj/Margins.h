f8d42fcf2004d0392aa30ef1d713c67eff9ce79f
up201405498 <NunoRamos@users.noreply.github.com>
2016-01-03 16:06:04 +0000

    Updating files

--- proj/Margins.h
+++ proj/Margins.h
@@ -1,32 +1,6 @@
 #ifndef __MARGINS_H
 #define __MARGINS_H
 
-//typedef struct {
-//	short x,y;
-//	short width,height;
-//	unsigned short distanceToOtherMargin;
-//	unsigned short totallyPrinted;  //if the margin is totally printed this variable will be equal to 1
-//} Margin;
-
-//typedef enum{
-//	FIRST_MARGIN_OF_THE_BREAK, REST_OF_MARGINS_AT_THE_BREAK
-//}GenerationState;
-//
-//Margin* newMargin(short x, short y, short width, short height);
-//
-//void delete_margin(Margin** margins,unsigned short *sizeOfArray);
-//
-//unsigned int tubeSize(unsigned int time);
-//
-//void draw_margin(Margin** margins,unsigned short *sizeOfArray,unsigned int time);
-//
-//int numberGeneration(int x1,int x2);
-//
-//Margin* randomMargin(short x,short y,unsigned int time);
-//
-//int isTotallyPrinted(Margin* margin);
-//
-//void pullToTheLeft(Margin** margins, unsigned short *sizeOfArray,unsigned int time);
 
 void firstImage();
 
